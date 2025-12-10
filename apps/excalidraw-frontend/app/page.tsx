a29f95b6765a23b12ef5cb896857e3b1327c74e2
Mohammed <mtajir903@gmail.com>
2025-12-10 07:30:24 +0530

    initialized excalidraw-frontend

---
+++ apps/excalidraw-frontend/app/page.tsx
@@ -0,0 +1,8 @@
+import AuthPage from "@/components/AuthPage";
+import Image from "next/image";
+
+export default function Home() {
+  return <div>
+    <AuthPage isSignIn={true} />
+  </div>
+}
---
+++ apps/excalidraw-frontend/components/AuthPage.tsx
@@ -0,0 +1,5 @@
+export default function AuthPage({ isSignIn } : { isSignIn : boolean }) {
+    return <div className="h-screen w-screen flex justify-center items-center">
+        hi there
+    </div>
+}
