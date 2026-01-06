ea2788f43d6b925c35d19603b9c6299b2c63182a
Mohammed <mtajir903@gmail.com>
2026-01-06 20:28:59 +0530

    debugging

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -3,9 +3,8 @@ dotenv.config();
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL || "",
 });
 
 
